d2f886a625fff159b175403b41a9b06abe173b49
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-02-24 14:10:57 -0700

    MVP

--- Segmented controllers/ViewController.swift
+++ Segmented controllers/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+   
+
+    @IBOutlet weak var hello: UILabel!
+    
+    
+    @IBOutlet weak var outlet: UISegmentedControl!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.blue]
+        
+        outlet.setTitleTextAttributes(titleTextAttributes, for: .normal)
+        
+        let titleTextAttributes2 = [NSAttributedString.Key.foregroundColor: UIColor.red]
+        
+        outlet.setTitleTextAttributes(titleTextAttributes2, for: .selected)
     }
-
-
+    
+    
+    @IBAction func language(_ sender: Any)
+    {
+        switch outlet.selectedSegmentIndex
+        
+        {
+        case 0:
+            hello.text = "Hello!"
+        
+        case 1:
+            hello.text = "Hola!"
+            
+        case 2:
+            hello.text = "Bonjour!"
+        default:
+            hello.text = "Hello!"
+        }
+        
+    }
+    
+    
 }
 
