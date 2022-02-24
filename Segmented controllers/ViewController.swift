e95e495274b79442a34fdca19f2f0e9cd413d32e
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-02-24 13:01:40 -0700

    Initial Commit

---
+++ Segmented controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segmented controllers
+//
+//  Created by Patrick Stroyan on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
